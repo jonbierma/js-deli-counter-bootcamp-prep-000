8932a74c1b9373d259c1d90501a1b185ef723db2
Jon Bierma <jon.bierma@gmail.com>
2017-09-13 20:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 function takeANumber(currentLine, newCustomer){
   currentLine.push(newCustomer);
   var position = currentLine.length
-  return `Welcome, ${newCustomer}. You are number ${position} in line`
+  return `Welcome, ${newCustomer}. You are number ${position} in line.`
 }
 
 function nowServing(currentLine){
   if (currentLine.length > 0){
     var currentCustomer = currentLine.shift()
-    return `Currently serving ${currentCustomer}`
+    return `Currently serving ${currentCustomer}.`
   } else {
     return "There is nobody waiting to be served!"
   }
@@ -20,8 +20,9 @@ function currentLine(line){
     return "The line is currently empty."
   } else {
     var lineString = "The line is currently: "
-    for (var i=0; i<line.length; i++){
-      lineString = lineString + `${i+1}. ${line[i]}, `
+    lineString = lineString + `1. ${line[0]}`
+    for (var i=1; i<line.length; i++){
+      lineString = lineString + `, ${i+1}. ${line[i]}`
     }
     return lineString
   }
